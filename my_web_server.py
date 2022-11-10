7a880f217ff4a8a597d8b0447f847d8209467cb5
nesterenko <sonia.nesterencko@yandex.ru>
2022-11-10 18:57:51 +0300

    API + Йолочка

--- my_web_server.py
+++ my_web_server.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
-import uvicorn
-app = FastAPI()
+# from fastapi import FastAPI
+# import uvicorn
+# from model import Model
+# app = FastAPI()
+# import numpy as np
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello World!"}
+# @app.get("/")
+# def root():
+#     return {"message": "Hello World!"}
 
 
 # @app.get("/api/")
@@ -13,14 +15,60 @@ def root():
 #     return {"api": 23}
 
 
-@app.get("/api/calculate/")
-def read_value(value_1: int, value_2: int):
-    return value_1 + value_2
+# @app.get("/api/calculate/")
+# def read_value(value_1: int, value_2: int):
+#     return value_1 + value_2
 
 
-@app.get("/api/calculate/")
-def read_value(value_1: int, value_2: int):
-    return value_1 + value_2
+# modelNN = model.Model()
+
+
+# @app.get("/api/model/")
+# def make_prediction():
+#     model = Model()
+#     model.preprocess()
+#     model.fit()
+#     model.chart()
+#     number = model.prediction()
+#
+#     return number
+
+
+# make_prediction()
+
 
 # uvicorn my_web_server:app --reload
+
+ #     ^
+ #    /__\
+ #   /____\
+ #  /______\
+ #
+
+ #     #
+ #    ####
+ #   ######
+
+
+class Yolochka:
+
+
+    def __init__(self):
+        ...
+
+    def draw(self):
+        width = 10
+        height = 4
+
+
+Yolochka.draw()
+
+n = int(input())
+
+for i in range(n):
+    while True:
+        print('/_')
+    for j in range(5):
+        if j == 1:
+            print("")
 
